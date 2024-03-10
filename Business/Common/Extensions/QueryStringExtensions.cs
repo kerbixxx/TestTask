9d9d365e289d9863c52eb43f0e8c94fa355e2970
Kerbix <kerbixcs@mail.ru>
2024-03-10 21:19:32 +0700

    Добавил фильтрацию и поиск

---
+++ Business/Common/Extensions/QueryStringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Common.Extensions
+{
+    public static class QueryStringExtensions
+    {
+        public static string ToQueryString(this IEnumerable<KeyValuePair<string, string>> parameters)
+        {
+            return string.Join("&", parameters.Select(kvp => $"{Uri.EscapeDataString(kvp.Key)}={Uri.EscapeDataString(kvp.Value)}"));
+        }
+    }
+}
